f2d2334e06abcd1c9fbcabebb7e689cf5ff6cdf1
tomridder <1402709211@qq.com>
2025-12-24 14:28:16 +0800

    add blog 2024 change to 2025

--- src/config/marketing.ts
+++ src/config/marketing.ts
@@ -2,10 +2,10 @@ import { MarketingConfig } from "@/types";
 
 export const marketingConfig: MarketingConfig = {
   mainNav: [
-    // {
-    //   title: "blog",
-    //   href: "/blog",
-    // },
+    {
+      title: "blog",
+      href: "/blog",
+    },
     {
       title: "generator-styles",
       href: "/generator-styles",
