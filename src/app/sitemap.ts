722f0c6b18ebd9c5acf98b978e404398f585e1f3
tomridder <1402709211@qq.com>
2025-12-24 14:33:33 +0800

    sitemap changed

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -29,6 +29,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
       url: `${baseUrl}/pricing`,
       lastModified: new Date(),
       changeFrequency: "weekly",
